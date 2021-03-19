0c994f25320fce98d943fd6ebf6d98ba6d2587ea
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-03-19 19:17:31 -0300

    Cadastro Finalizado

---
+++ config/dbConnection.js
@@ -0,0 +1,21 @@
+/** Importar o mongodb */
+var mongo = require("mongodb");
+
+var connMongoDB = function () {
+  console.log("Entrou na função de conexão");
+  var db = new mongo.Db(
+    "got",
+    new mongo.Server(
+      "localhost", //String contendo o endereço do servidor
+      27017, //porta de conexão
+      {}
+    ),
+    {}
+  );
+
+  return db;
+};
+
+module.exports = function () {
+  return connMongoDB;
+};
