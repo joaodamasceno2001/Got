cd587e14d55b19759ee0654f8a59a32bc0be7499
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-03-24 18:12:05 -0300

    Show abilities points

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -7,7 +7,6 @@ function UsuariosDAO(connection) {
 UsuariosDAO.prototype.inserirUsuario = function (usuario) {
   this._connection.open(function (err, mongoclient) {
     mongoclient.collection("usuarios", function (err, collection) {
-      console.log(usuario);
       collection.insert(usuario);
 
       mongoclient.close();
