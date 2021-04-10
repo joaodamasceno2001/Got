dcad73cf1c3bcd542ae54e068a9408c5470f6f36
João Victor Damasceno <joao_damasceno2001@hotmail.com>
2021-04-10 12:32:01 -0300

    comentario

--- app/routes/jogo.js
+++ app/routes/jogo.js
@@ -3,6 +3,7 @@ module.exports = function (application) {
     application.app.controllers.jogo.jogo(application, req, res);
   });
 
+  // Rota de sair
   application.get("/sair", function (req, res) {
     application.app.controllers.jogo.sair(application, req, res);
   });
